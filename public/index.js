25d6952034ec728beaac21e95421e2b7b958e878
ArtMatvienco <artmatvienco@gmail.com>
2024-03-15 01:48:34 +0200

    add: dark and light theme, changes img

--- public/index.js
+++ public/index.js
@@ -2,3 +2,13 @@ document.getElementById('burger-menu').addEventListener('click', function () {
   const mobileMenu = document.getElementById('mobile-menu');
   mobileMenu.classList.toggle('hidden');
 });
+
+const checkbox = document.querySelector('#toggle');
+const html = document.querySelector('html');
+
+const toggleDarkMode = function () {
+  checkbox.checked ? html.classList.add('dark') : html.classList.remove('dark');
+};
+
+toggleDarkMode();
+checkbox.addEventListener('click', toggleDarkMode);
